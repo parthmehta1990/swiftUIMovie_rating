3c5befcfa9c9a47804d74e94692191126d8d6211
parthmehta <parth1990mehta@gmail.com>
2026-04-06 16:13:42 +0530

    Created the UI for the movie rating app

--- iosMovieApp/ContentView.swift
+++ iosMovieApp/ContentView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var title=""
+    @State var rating:Double=3.0
+    @State var seen:Bool=false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(){
+                Section{
+                    SectionTitle(titleText: "Title")
+                    TextField("Search", text: $title)
+                }
+                Section{
+                    SectionTitle(titleText: "Rating")
+                    HStack{
+                        Spacer()
+                        Text(String(repeating: "★", count: Int(rating))).foregroundStyle(Color.yellow).font(.title)
+                        Spacer()
+                    }
+                    Slider(value: $rating,in: 1...5,step: 1)
+                }
+                Section{
+                    SectionTitle(titleText:"Seen the movie?")
+                    Toggle(isOn: $seen){
+                        if title == ""{
+                            Text("I have seen this movie")
+                        }
+                        else{
+                            Text("I have seen \(title) ")
+                        }
+                    }
+                }
+                
+                Section{
+                    Button(action:{}){
+                        HStack{
+                            Spacer()
+                            Text("save")
+                            Spacer()
+                        }
+                    }
+                }
+                
+            }.listStyle(GroupedListStyle())
         }
         .padding()
     }
@@ -21,4 +59,11 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct SectionTitle:View {
+    var titleText:String
+    var body: some View {
+        Text(titleText).font(.caption).foregroundStyle(.gray)
+    }
 }
