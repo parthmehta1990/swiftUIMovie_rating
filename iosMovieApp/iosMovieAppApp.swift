0527d6209af21994450a604161074ecc8593e0a5
parthmehta <parth1990mehta@gmail.com>
2026-04-06 11:20:38 +0530

    Initial Commit

---
+++ iosMovieApp/iosMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosMovieAppApp.swift
+//  iosMovieApp
+//
+//  Created by Parth Mehta on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct iosMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
